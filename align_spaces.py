6845f5996c22c3ae4881cec8f119d0aaefe88904
scwatts <s.watts2@student.unimelb.edu.au>
2018-02-07 11:39:35 +1100

    Hello

---
+++ align_spaces.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import argparse
+import pathlib
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_fp', required=True, type=pathlib.Path,
+            help='Input file path containing crispr spacers')
+
+    args = parser.parse_args()
+    if not args.input_fp.exists():
+        parser.error('Input file %s does not exist' % args.input_fp)
+
+    return args
+
+
+def main():
+    # Get command line arguments
+    args = get_arguments()
+
+    # Read in spacers
+    with args.input_fp.open('r') as fh:
+        line_token_gen = (line.rstrip().split() for line in fh)
+        spacers = {name: spacers for name, *spacers in line_token_gen}
+
+    print(spacers)
+
+
+if __name__ == '__main__':
+    main()
