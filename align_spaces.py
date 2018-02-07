636f30f0663856ed615798c0a867c804a32d824f
Alex Tokolyi <alex.tokolyi@gmail.com>
2018-02-07 13:05:03 +1100

    Handling input and construction of graph

--- align_spaces.py
+++ align_spaces.py
@@ -5,7 +5,6 @@ import pathlib
 
 import igraph
 
-
 def get_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('--input_fp', required=True, type=pathlib.Path,
@@ -26,23 +25,25 @@ def main():
     with args.input_fp.open('r') as fh:
         line_token_gen = (line.rstrip().split() for line in fh)
         all_spacers = {name: spacers for name, *spacers in line_token_gen}
-
-
-    # TODO: consider node input space and add prior
-    # Generate graph structure
+   
+    # Create the graph, adding vertice
     graph = igraph.Graph(directed=True)
-    for name, spacers in all_spacers:
-        # Get all edge pairs
-        spacer_iter = (spacers[i:i+2] for i in range(len(spacers)))
-        edge_gen = (e for e in spacer_iter if len(e) > 1)
+    vert_set = set()
+    for x in all_spacers:
+        for y in all_spacers[x]:
+            vert_set.add(y)
+    for x in vert_set:
+        graph.add_vertex(x)
 
-        # Add to graph
-        for source_node, target_node for edge_gen:
-            try:
-                graph.add_edge(source_node, target_node)
-            except igraph._igraph.InternalError:
-                if
-                graph.add_vertex(
+    # Add vertices from input 
+    for x in all_spacers:
+        for i in range(len(all_spacers[x])-1):
+            graph.add_edge(all_spacers[x][i],all_spacers[x][i+1])
+    
+    # Print out representation
+    print(graph)
+    print(*graph.get_adjacency(), sep='\n')
+
 
     # Try to run topo sorting
 
