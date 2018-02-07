39313b20346b9c6c18c508a757d328a53a0d4ca0
scwatts <s.watts2@student.unimelb.edu.au>
2018-02-07 15:27:30 +1100

    Generate alignment in stdout

--- align_spaces.py
+++ align_spaces.py
@@ -5,6 +5,7 @@ import pathlib
 
 import igraph
 
+
 def get_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('--input_fp', required=True, type=pathlib.Path,
@@ -26,6 +27,21 @@ def main():
         line_token_gen = (line.rstrip().split() for line in fh)
         all_spacers = {name: spacers for name, *spacers in line_token_gen}
 
+    # Create graph
+    graph = generate_graph(all_spacers)
+
+    # Get topological order, remove edges to demote graph to acyclic if required
+    order_indices = get_spacer_order(graph)
+    node_names = list(graph.vs)
+    order_names = [node_names[i]['name'] for i in order_indices]
+
+    # Create spacer alignment using order indices
+    alignment = create_alignment(all_spacers, order_names)
+    for name, ordered_spacers in alignment.items():
+        print(name, *ordered_spacers, sep='\t')
+
+
+def generate_graph(all_spacers):
     # Create the graph, adding vertice
     graph = igraph.Graph(directed=True)
     vert_set = set()
@@ -40,22 +56,31 @@ def main():
         for i in range(len(all_spacers[x])-1):
             graph.add_edge(all_spacers[x][i],all_spacers[x][i+1])
 
-    # Try to run topo sorting. If this fails because graph is cyclic, remove nodes to restor
+    return graph
+
+
+def get_spacer_order(graph):
+    # Try to run topo sorting. If this fails because graph is cyclic, remove nodes to restore
     # acyclicness
-    # TODO: clean this up
-    ordering = graph.topological_sorting()
-    if len(ordering) != len(graph.vs()):
+    # TODO: clean this up?
+    order_indices = graph.topological_sorting()
+    if len(order_indices) != len(graph.vs()):
         rnodes = graph.feedback_arc_set()
         graph.delete_edges(graph.feedback_arc_set())
-    ordering = graph.topological_sorting()
+        return graph.topological_sorting()
+    else:
+        return order_indices
 
-    # Order spacer lists with determined order
-    # TODO: see output of ordering and then apply
-    print(ordering)
-    print(*(list(graph.vs)[i]['name'] for i in ordering))
-    print(*graph.get_adjacency(), sep='\n')
 
-    # Print out results, for now
+def create_alignment(all_spacers, order_names):
+    # Order spacers lists with determined order
+    ordered_spacers = dict()
+    for name, spacers in all_spacers.items():
+        order_dict = {s: '-' for s in order_names}
+        for spacer in spacers:
+            order_dict[spacer] = spacer
+        ordered_spacers[name] = list(order_dict.values())
+    return ordered_spacers
 
 
 if __name__ == '__main__':
